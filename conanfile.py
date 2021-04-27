a98fc8cc7db33e92951a2db78e08a2777334dfce
m-spiessens <mathias@spiessens.eu>
2021-04-27 10:35:20 +0200

    Support for Cortex-M33.

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class Flow(ConanFile):
 	license = "MIT"
 	author = "Mathias Spiessens"
 	build_policy = "missing"
-	settings = { "arch": ["x86", "x86_64", "armv6", "armv7", "armv7hf"], "os": ["none", "Linux"], "build_type": ["Release", "Debug"], "compiler": ["gcc"] }
+	settings = { "arch": ["x86", "x86_64", "armv6", "armv7", "armv7hf", "armv8_32"], "os": ["none", "Linux"], "build_type": ["Release", "Debug"], "compiler": ["gcc"] }
 	generators = "cmake"
 	exports_sources = "include/*", "source/*", "CMakeLists.txt"
 
@@ -21,11 +21,13 @@ class Flow(ConanFile):
 			cmake.definitions["CMAKE_TRY_COMPILE_TARGET_TYPE"] = "STATIC_LIBRARY"
 
 		if self.settings.arch == "armv6":
-			cmake.definitions["CMAKE_CXX_FLAGS_INIT"] = "-march=armv6-m -mthumb -fno-exceptions"
+			cmake.definitions["CMAKE_CXX_FLAGS"] = "-march=armv6-m -mthumb -fno-rtti -fno-exceptions -fdata-sections -ffunction-sections"
 		elif self.settings.arch == "armv7":
-			cmake.definitions["CMAKE_CXX_FLAGS_INIT"] = "-march=armv7e-m -mthumb -fno-exceptions"
+			cmake.definitions["CMAKE_CXX_FLAGS"] = "-march=armv7e-m -mthumb -fno-rtti -fno-exceptions -fdata-sections -ffunction-sections"
 		elif self.settings.arch == "armv7hf":
-			cmake.definitions["CMAKE_CXX_FLAGS_INIT"] = "-march=armv7e-m -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -fno-exceptions"
+			cmake.definitions["CMAKE_CXX_FLAGS"] = "-march=armv7e-m -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -fno-rtti -fno-exceptions -fdata-sections -ffunction-sections"
+		elif self.settings.arch == "armv8_32":
+			cmake.definitions["CMAKE_CXX_FLAGS"] = "-march=armv8-m.main -mthumb -fno-rtti -fno-exceptions -fdata-sections -ffunction-sections"
 
 		cmake.configure()
 		cmake.build()
@@ -41,7 +43,7 @@ class Flow(ConanFile):
 
 		if self.settings.arch == "x86" or self.info.settings.arch == "x86_64":
 			self.copy("platform_cpputest.cpp", "source/flow/", "source/flow/")
-		elif self.settings.arch == "armv6" or self.settings.arch == "armv7" or self.settings.arch == "armv7hf":
+		elif self.settings.arch == "armv6" or self.settings.arch == "armv7" or self.settings.arch == "armv7hf" or self.settings.arch == "armv8_32":
 			self.copy("platform_cortexm4.cpp", "source/flow/", "source/flow/")
 		else:
 			raise Exception("Invalid settings for this package.")
